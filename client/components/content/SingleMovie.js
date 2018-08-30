cfbc3557529bf22adb94f1867a8b4877ad05a0ae
Mahiyaa <mmahia1121@gmail.com>
2018-08-29 22:16:00 -0400

    Single movie skeleton

---
+++ client/components/content/SingleMovie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SingleMovie extends Component {
+  constructor() {
+      super();
+      this.state = {
+
+      }
+  }
+
+  componentDidMount(){
+    this.props.getMovie();
+
+}
+
+render() {
+  }
+}
